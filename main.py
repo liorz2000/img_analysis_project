c2e87ea9ea4aa9420fdddb863cade6390464f00d
liorz2000 <liorz2000@gmail.com>
2022-08-20 10:24:37 +0300

    Add get_imgs

--- main.py
+++ main.py
@@ -7,9 +7,30 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+PATH_C1 = "../images/defective_examples/case1_{}_image.tif"
+PATH_C2 = "../images/defective_examples/case2_{}_image.tif"
+PATH_C3 = "../images/non_defective_examples/case3_{}_image.tif"
+paths = [PATH_C1,PATH_C2,PATH_C3]
+ins = "inspected"
+ref = "reference"
+
+def get_imgs():
+	"""
+	Get the 6 images as 3X2 matrix.
+	Each row for each case.	
+	"""
+    res = []
+    for i in range(3):
+        res.append([])
+        path_ins = paths[i].format(ins)
+        path_ref = paths[i].format(ref)
+        res[-1].append(np.array(cv2.imread(path_ref, cv2.IMREAD_GRAYSCALE)))
+        res[-1].append(np.array(cv2.imread(path_ins, cv2.IMREAD_GRAYSCALE)))
+    return res
+
 def get_polynom_mult(img_reference , img_inspected):
     """
-    inputs must be 2 numpy arrays represent gray-scale 0-255
+    Inputs must be 2 numpy arrays represent gray-scale 0-255
     """
     img_inspected = img_inspected[::-1,::-1]
     rows, colums = img_reference.shape
