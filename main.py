c198db4303d38880f64157a087017842947fb876
liorz2000 <liorz2000@gmail.com>
2022-08-26 13:33:51 +0300

    Add possability to diff imgs without abs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from help_funcs import *
 
-def get_dict_of_the_proccess(move_search = "normal", imgs = "the_three_given"):
+def get_dict_of_the_proccess(prev_path = "../", move_search = "normal", imgs = "the_three_given"):
 	"""
 	type value need to be one of those:
 		"normal": just do furie
@@ -11,7 +11,7 @@ def get_dict_of_the_proccess(move_search = "normal", imgs = "the_three_given"):
 		raise Exception("No valid move_search")
 
 	if imgs == "the_three_given":
-		imgs = get_imgs()
+		imgs = get_imgs(prev_path)
 	if move_search == "normal":
 		mults = get_mults(imgs)
 		move_vectors = get_max_vectors(mults)
@@ -22,12 +22,14 @@ def get_dict_of_the_proccess(move_search = "normal", imgs = "the_three_given"):
 			mults = add_margin_normalization_to_padded_mults(mults, exp_muls)
 		move_vectors = get_max_vectors(mults, True)
 	cutted_imgs = cut_all_mergins(imgs, move_vectors)
-	diff_imgs = sub_all_imgs(cutted_imgs)
+	diff_imgs = sub_all_imgs(cutted_imgs, False)
+	diff_abs_imgs = sub_all_imgs(cutted_imgs)
 	correlations = find_correlations(cutted_imgs)
-	
+
 	dct = {"mults":mults, "move_vectors":move_vectors}
 	dct["cutted_imgs"] = cutted_imgs
 	dct["diff_imgs"] = diff_imgs
+	dct["diff_abs_imgs"] = diff_abs_imgs
 	dct["correlations"] = correlations
 	dct["move_search"] = move_search
 	return dct
