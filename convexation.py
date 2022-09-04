26460296944666886e90ba0177aa97018f3b4686
liorz2000 <liorz2000@gmail.com>
2022-09-04 08:39:39 +0300

    add documentation

--- convexation.py
+++ convexation.py
@@ -7,3 +7,63 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from itertools import product
+
+from separation import *
+
+def convexation(img, raduis):
+	"""
+	@img (np.matrix):
+	Image of ones and zeros, represent if pixel is defected.
+	@raduis (int):
+	We assume that spots are convex.
+	So if there is pixel above and below that detected as defected, 
+	it will be detected as defected as well. The same with right-left.
+	It is the raduis of finding the neighbor pixels, in these four directions.
+
+	@result (np.matrix):
+	The image after convexetion accord to the raduis.
+	"""
+    down = np.zeros(img.shape)
+    up = np.zeros(img.shape)
+    right = np.zeros(img.shape)
+    left = np.zeros(img.shape)
+    
+    for i in range(1, raduis+1):
+        down[i][0] = 1
+        up[-i][0] = 1
+        right[0][i] = 1
+        left[0][-i] = 1
+    
+    is_down = mean_two_mat(img,down)
+    is_up = mean_two_mat(img,up)
+    is_right = mean_two_mat(img,right)
+    is_left = mean_two_mat(img,left)
+    
+    img_copy =np.copy(img)
+    for row in range(img.shape[0]):
+        for col in range(img.shape[1]):
+            if not img[row][col]:
+                if is_down[row][col]:
+                    if is_up[row][col]:
+                        img_copy[row][col] = 1
+                if is_right[row][col]:
+                    if is_right[row][col]:
+                        img_copy[row][col] = 1
+    return img_copy
+
+def convexation_list_dict_dict(ldd, raduis):
+	"""
+	@ ldd (list of dict of dict of np.matrix):
+	list represent diffrent tuples of ref-ins.
+	dict represent diffrent means.
+	dict represent diffrent cuts. 
+	@ raduis (int): read the explenation at the above func.
+
+	@ result (list of dict of dict of np.matrix):
+	Activate convexation on each np.matrix.
+	"""
+    for case in ldd:
+    	for local_mean_type in case:
+    		for cut in case[local_mean_type]:
+    			case[local_mean_type][cut] = convexation(case[local_mean_type][cut], raduis)
+    return ldd
--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 from move_find import *
 from separation import *
+from convexation import *
 
 def get_dict_of_the_proccess(prev_path = "../", move_search = "normal", imgs = "the_three_given",\
-	means_arr = [], cuts_r = [], cuts_a = []):
+	means_arr = [], cuts_r = [], cuts_a = [], conv_raduis = 3):
 	"""
-	type value need to be one of those:
+	@ prev_path (string):
+	In case the user didn't gave pictures, 
+	It is the relative path to images directory of the three images
+	@ move_search (string): 
+	    The algorithem to find the move vector between pictures.  
+		value need to be one of those:
 		"normal": just do furie
 		"pad": furie with double zero paddig
 		"pad_and_add": prev + adding repairs
+	@ imgs (matrix of np.matrix or string):
+	represent the pictures that the alorithem will work on.
+	if string: it must be "the_three_given", represent
+	the three cupules of ins-ref images that Spais gave.
+	if matrix: then it must be 2xn, whitch are n couples
+	of ins-ref images.
+	@ means_arr (array of strings): 
+	The names of neighberhoods type to do mean around pixel.
+	For each image in diff_imgsXmean_type it calculate the 
+	mean of the diff.
+	@ cuts_r (array of int):
+	The relative cuts To devide picture to good and defected pixels.
+	For each diff_imgsXmean_typeXcut it take the abs(the mean of the diff) 
+	and then separate it to pixels above max*cut and below. 
+	@ cuts_a (array of int):The relative cuts To devide picture to good and bad pixels.
+	For each diff_imgsXmean_typeXcut it take the abs(the mean of the diff) 
+	and then separate it to pixels above cut and below. 
+	@ conv_raduis (int): 
+	In this stage we assume that spots are convex.
+	So if there is pixel above and below that detected as defected, 
+	it will be detected as defected as well. The same with right-left.
+	The conv_raduis is the raduis of finding the neighbor pixels, 
+	in these four directions.
+
+	@result (dict): every detaile in the process. 
+
 	"""
 	if move_search not in ["normal", "pad" , "pad_and_add"]:
 		raise Exception("No valid move_search")
@@ -32,6 +64,10 @@ def get_dict_of_the_proccess(prev_path = "../", move_search = "normal", imgs = "
 	diff_means_abs_separations = separation_dicts_of_imgs(diff_means_abs, cuts_r, cuts_a)
 	diff_means_abs_separations_relative = diff_means_abs_separations[0]
 	diff_means_abs_separations_absolute = diff_means_abs_separations[1]
+	diff_means_abs_separations_relative_con = \
+	convexation_list_dict_dict(diff_means_abs_separations_relative, conv_raduis)
+	diff_means_abs_separations_absolute_con = \
+	convexation_list_dict_dict(diff_means_abs_separations_absolute, conv_raduis)
 
 
 	dct = {"mults":mults, "move_vectors":move_vectors}
@@ -45,12 +81,21 @@ def get_dict_of_the_proccess(prev_path = "../", move_search = "normal", imgs = "
 	dct["diff_means_abs"] = diff_means_abs
 	dct["diff_means_abs_separations_relative"] = diff_means_abs_separations_relative
 	dct["diff_means_abs_separations_absolute"] = diff_means_abs_separations_absolute
+	dct["diff_means_abs_separations_relative_con"] = diff_means_abs_separations_relative_con
+	dct["diff_means_abs_separations_absolute_con"] = diff_means_abs_separations_absolute_con
 
 	return dct
 
 def solution(img1, img2):
+	"""
+	@img1 (np.matrix) reference image
+	@img2 (np.matrix): inspected image
+
+	@result(np.matrix): The 1-0 image that requested,
+	1 for defected pixel and 0 for good.
+	"""
 	imgs = [[img1, img2]]
 	cut = 84
 	local_mean_type = "3x3"
 	dct = get_dict_of_the_proccess(imgs = imgs, means_arr = [local_mean_type], cuts_a = [cut])
-	return dct['diff_means_abs_separations_absolute'][0][local_mean_type][cut]
+	return dct["diff_means_abs_separations_absolute_con"][0][local_mean_type][cut]
